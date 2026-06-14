966b641da7518059d7953c3436056a91a266339e
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-06-13 23:46:39 -0400

    Update script-adm.sh

--- script-adm.sh
+++ script-adm.sh
@@ -47,7 +47,7 @@ cat > /etc/dealer-adm/banner.txt << BANNER
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#ff0000"><b>Expira:</b></font>
 <font color="#ff0000">$EXP_SHOW</font><br>
 
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#0095b6">❒══════</font><font color="#00ff00"> Script Dealer Adm </font><font color="#00ff00">══════❒</font>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="#0095b6">❒══════</font><font color="#00ff00"> Script Dealer Adm </font><font color="#ff00ff">══════❒</font>
 BANNER
 
 exit 0
