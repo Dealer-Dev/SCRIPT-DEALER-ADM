1eae8df44b08cbfa378eecac487ce82d9b41317d
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-06-13 22:23:44 -0400

    Update script-adm.sh

--- script-adm.sh
+++ script-adm.sh
@@ -37,21 +37,20 @@ FILE="/etc/dealer-adm/userDIR/$USR"
 EXP=$(grep '^fecha:' "$FILE" | awk '{print $2}')
 
 cat > /etc/dealer-adm/banner.txt << BANNER
-<center>
 <font color="#0095b6"><b>Usuario:</b></font>
 <font color="#0095b6">$USR</font>
 
-<br><br>
+<br>
 
 <font color="#ff8000"><b>Expira:</b></font>
 <font color="#ff0000">$EXP</font>
 
-<br><br><br>
+<br>
 
-<font color="#00ff00">
-<b>Script Dealer Adm</b>
-</font>
+<center>
+<font color="#00ff00"><b>Script Dealer Adm</b></font>
 </center>
+
 BANNER
 
 exit 0
