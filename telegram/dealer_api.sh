0de6e00d6cf0ce78233d2c06b78d862065895c7c
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-06-16 13:07:13 -0400

    Update dealer_api.sh

--- telegram/dealer_api.sh
+++ telegram/dealer_api.sh
@@ -1,3 +1,41 @@
-crear_usuario_ssh_api
-crear_usuario_hwid_api
-crear_usuario_token_api
+#!/bin/bash
+
+source /usr/local/bin/menu >/dev/null 2>&1
+
+ACCION="$1"
+
+case "$ACCION" in
+
+    agregar)
+        crear_usuario_ssh_api "$2" "$3" "$4" "$5"
+    ;;
+
+    token)
+        crear_usuario_token_api "$2" "$3" "$4"
+    ;;
+
+    hwid)
+        crear_usuario_hwid_api "$2" "$3" "$4"
+    ;;
+
+    renovar)
+        renovar_usuario_api "$2" "$3"
+    ;;
+
+    eliminar)
+        eliminar_usuario_api "$2"
+    ;;
+
+    usuarios)
+        listar_usuarios_api
+    ;;
+
+    online)
+        usuarios_online_api
+    ;;
+
+    *)
+        echo "ERROR"
+    ;;
+
+esac
