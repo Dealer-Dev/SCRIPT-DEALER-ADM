04a33b8398bac11f3381b3378153020927f952cf
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-06-16 22:22:57 -0400

    Update dealer_api.sh

--- telegram/dealer_api.sh
+++ telegram/dealer_api.sh
@@ -9,31 +9,86 @@ ACCION="$1"
 case "$ACCION" in
 
     agregar)
-        crear_usuario_ssh_api "$2" "$3" "$4" "$5"
+
+        # agregar user pass dias limite admin_id admin_nombre
+
+        crear_usuario_ssh_api \
+        "$2" \
+        "$3" \
+        "$4" \
+        "$5" \
+        "$6" \
+        "$7"
+
     ;;
 
     token)
-        crear_usuario_token_api "$2" "$3" "$4"
+
+        # token nombre token dias admin_id admin_nombre
+
+        crear_usuario_token_api \
+        "$2" \
+        "$3" \
+        "$4" \
+        "$5" \
+        "$6"
+
     ;;
 
     hwid)
-        crear_usuario_hwid_api "$2" "$3" "$4"
+
+        # hwid nombre hwid dias admin_id admin_nombre
+
+        crear_usuario_hwid_api \
+        "$2" \
+        "$3" \
+        "$4" \
+        "$5" \
+        "$6"
+
     ;;
 
     renovar)
-        renovar_usuario_api "$2" "$3"
+
+        # renovar usuario dias admin_id owner_id
+
+        renovar_usuario_api \
+        "$2" \
+        "$3" \
+        "$4" \
+        "$5"
+
     ;;
 
     eliminar)
-        eliminar_usuario_api "$2"
+
+        # eliminar usuario admin_id owner_id
+
+        eliminar_usuario_api \
+        "$2" \
+        "$3" \
+        "$4"
+
     ;;
 
     usuarios)
-        listar_usuarios_api
+
+        # usuarios admin_id owner_id
+
+        listar_usuarios_api \
+        "$2" \
+        "$3"
+
     ;;
 
     online)
-        usuarios_online_api
+
+        # online admin_id owner_id
+
+        usuarios_online_api \
+        "$2" \
+        "$3"
+
     ;;
 
     *)
