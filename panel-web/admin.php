190d2935df232dd8330670a4a5ca74dd04f82c93
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2026-08-01 02:53:39 -0400

    Update admin.php

--- panel-web/admin.php
+++ panel-web/admin.php
@@ -297,7 +297,7 @@ $resellers = $conn->query("SELECT * FROM users WHERE role='reseller' ORDER BY id
 <!DOCTYPE html>
 <html>
 <head>
-<title>Admin - Panel Local VPS</title>
+<title>Admin - Panel Dealer</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <style>
 *{box-sizing:border-box;}
